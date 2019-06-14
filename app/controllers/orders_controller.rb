abba0263115d463955d6ec624b430729ad09d922
Daniel <nhanxtrand@gmail.com>
2019-06-14 15:26:07 +0000

    Render products from orders table

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,14 +3,14 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
-
+  
   def create
     charge = perform_stripe_charge
     order  = create_order(charge)
-
+    
     if order.valid?
-      redirect_to order, notice: 'Your Order has been placed.'
       empty_cart!
+      redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
